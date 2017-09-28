bc7a5568c7943139e66d2d19fdb8c1f4c0927d42
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-28 02:07:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,30 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+    $('img').css("border","1px solid red");
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("You've pressed the G key.")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
